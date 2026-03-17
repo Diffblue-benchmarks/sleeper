31038bca95f568044771d3f5b5730d5d99a354bb
Diffblue CI <ci@diffblue.com>
2026-03-17 10:38:14 +0000

    test(compaction-core): Add generated tests for sleeper.compaction.core.job.dispatch.CompactionJobBatchExpiredException.<init>(Lsleeper/compaction/core/job/dispatch/CompactionJobDispatchRequest;Ljava/time/Instant;)V

---
+++ java/compaction/compaction-core/src/test/java/sleeper/compaction/core/job/dispatch/CompactionJobBatchExpiredExceptionTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022-2024 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sleeper.compaction.core.job.dispatch;
+
+import org.junit.jupiter.api.Test;
+
+import sleeper.core.properties.instance.InstanceProperties;
+import sleeper.core.properties.table.TableProperties;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static sleeper.core.properties.instance.CdkDefinedInstanceProperty.DATA_BUCKET;
+import static sleeper.core.properties.table.TableProperty.TABLE_ID;
+import static sleeper.core.properties.testutils.InstancePropertiesTestHelper.createTestInstanceProperties;
+import static sleeper.core.properties.testutils.TablePropertiesTestHelper.createTestTablePropertiesWithNoSchema;
+
+public class CompactionJobBatchExpiredExceptionTest {
+
+    @Test
+    void shouldConstructExceptionMessageWithTableIdBatchKeyAndExpiryTime() {
+        // Given
+        InstanceProperties instanceProperties = createTestInstanceProperties();
+        instanceProperties.set(DATA_BUCKET, "test-bucket");
+        TableProperties tableProperties = createTestTablePropertiesWithNoSchema(instanceProperties);
+        tableProperties.set(TABLE_ID, "test-table");
+        CompactionJobDispatchRequest request = CompactionJobDispatchRequest.forTableWithBatchIdAtTime(
+                tableProperties, "test-batch", Instant.parse("2024-11-18T12:01:00Z"));
+        Instant expiryTime = Instant.parse("2024-11-18T13:01:00Z");
+
+        // When
+        CompactionJobBatchExpiredException exception = new CompactionJobBatchExpiredException(request, expiryTime);
+
+        // Then
+        assertThat(exception.getMessage())
+                .contains("test-table")
+                .contains("test-batch")
+                .contains("2024-11-18T13:01:00Z");
+    }
+}
