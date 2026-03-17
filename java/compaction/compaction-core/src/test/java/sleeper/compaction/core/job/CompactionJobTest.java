27c7df49df83f82e68a671261f05ddec86caae6b
Diffblue CI <ci@diffblue.com>
2026-03-17 10:31:07 +0000

    test(compaction-core): Add generated tests for sleeper.compaction.core.job.CompactionJob$Builder
    - <init>
    - tableId
    - jobId
    - inputFiles
    - outputFile
    - partitionId
    - iteratorClassName
    - iteratorConfig
    - build

--- java/compaction/compaction-core/src/test/java/sleeper/compaction/core/job/CompactionJobTest.java
+++ java/compaction/compaction-core/src/test/java/sleeper/compaction/core/job/CompactionJobTest.java
@@ -147,4 +147,137 @@ public class CompactionJobTest {
         assertThatThrownBy(jobBuilder::build)
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    public void shouldBuildCompactionJobWithAllFields() {
+        // Given
+        CompactionJob.Builder builder = CompactionJob.builder()
+                .tableId("test-table")
+                .jobId("test-job")
+                .inputFiles(Arrays.asList("file1", "file2"))
+                .outputFile("output")
+                .partitionId("partition1")
+                .iteratorClassName("IteratorClass")
+                .iteratorConfig("config");
+
+        // When
+        CompactionJob job = builder.build();
+
+        // Then
+        assertThat(job.getTableId()).isEqualTo("test-table");
+        assertThat(job.getId()).isEqualTo("test-job");
+        assertThat(job.getInputFiles()).containsExactly("file1", "file2");
+        assertThat(job.getOutputFile()).isEqualTo("output");
+        assertThat(job.getPartitionId()).isEqualTo("partition1");
+        assertThat(job.getIteratorClassName()).isEqualTo("IteratorClass");
+        assertThat(job.getIteratorConfig()).isEqualTo("config");
+    }
+
+    @Test
+    public void shouldBuildCompactionJobWithMinimalFields() {
+        // Given
+        CompactionJob.Builder builder = CompactionJob.builder()
+                .tableId("test-table")
+                .jobId("test-job")
+                .inputFiles(Arrays.asList("file1"))
+                .outputFile("output")
+                .partitionId("partition1");
+
+        // When
+        CompactionJob job = builder.build();
+
+        // Then
+        assertThat(job.getTableId()).isEqualTo("test-table");
+        assertThat(job.getId()).isEqualTo("test-job");
+        assertThat(job.getInputFiles()).containsExactly("file1");
+        assertThat(job.getOutputFile()).isEqualTo("output");
+        assertThat(job.getPartitionId()).isEqualTo("partition1");
+        assertThat(job.getIteratorClassName()).isNull();
+        assertThat(job.getIteratorConfig()).isNull();
+    }
+
+    @Test
+    public void shouldReturnBuilderFromTableIdMethod() {
+        // Given
+        CompactionJob.Builder builder = CompactionJob.builder();
+
+        // When
+        CompactionJob.Builder result = builder.tableId("table-id");
+
+        // Then
+        assertThat(result).isSameAs(builder);
+    }
+
+    @Test
+    public void shouldReturnBuilderFromJobIdMethod() {
+        // Given
+        CompactionJob.Builder builder = CompactionJob.builder();
+
+        // When
+        CompactionJob.Builder result = builder.jobId("job-id");
+
+        // Then
+        assertThat(result).isSameAs(builder);
+    }
+
+    @Test
+    public void shouldReturnBuilderFromInputFilesMethod() {
+        // Given
+        CompactionJob.Builder builder = CompactionJob.builder();
+        List<String> files = Arrays.asList("file1", "file2");
+
+        // When
+        CompactionJob.Builder result = builder.inputFiles(files);
+
+        // Then
+        assertThat(result).isSameAs(builder);
+    }
+
+    @Test
+    public void shouldReturnBuilderFromOutputFileMethod() {
+        // Given
+        CompactionJob.Builder builder = CompactionJob.builder();
+
+        // When
+        CompactionJob.Builder result = builder.outputFile("output");
+
+        // Then
+        assertThat(result).isSameAs(builder);
+    }
+
+    @Test
+    public void shouldReturnBuilderFromPartitionIdMethod() {
+        // Given
+        CompactionJob.Builder builder = CompactionJob.builder();
+
+        // When
+        CompactionJob.Builder result = builder.partitionId("partition-id");
+
+        // Then
+        assertThat(result).isSameAs(builder);
+    }
+
+    @Test
+    public void shouldReturnBuilderFromIteratorClassNameMethod() {
+        // Given
+        CompactionJob.Builder builder = CompactionJob.builder();
+
+        // When
+        CompactionJob.Builder result = builder.iteratorClassName("IteratorClass");
+
+        // Then
+        assertThat(result).isSameAs(builder);
+    }
+
+    @Test
+    public void shouldReturnBuilderFromIteratorConfigMethod() {
+        // Given
+        CompactionJob.Builder builder = CompactionJob.builder();
+
+        // When
+        CompactionJob.Builder result = builder.iteratorConfig("config");
+
+        // Then
+        assertThat(result).isSameAs(builder);
+    }
 }
